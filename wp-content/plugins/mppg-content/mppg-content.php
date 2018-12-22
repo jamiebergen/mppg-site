014eece4b000956f058e1fff41531da7c2cbe195
Jamie Bergen <jbergen1@sls-134-mbpro15.local>
2018-12-21 21:18:45 -0800

    Initial commit for MPPG Content plugin

---
+++ wp-content/plugins/mppg-content/mppg-content.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     MPPG Front-End Content
+ * Plugin URI:      https://jamiebergen.com/
+ * Description:     Utility plugin for registering content types, taxonomies, data handling, and front-end submission functionality
+ * Author:          Jamie Bergen
+ * Author URI:      https://jamiebergen.com/
+ * Text Domain:     mppg-content
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Mppg_Content
+ */
+
+// Your code starts here.
